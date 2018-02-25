22007cacda5dd5970e17a70500b18ba1a2140979
BitesTheDust <2jaybel@gmail.com>
2018-02-25 23:29:54 +0200

    Fixed ShootState.
    Added more conditions to ChangeState.

--- Assets/Code/AI/ShootState.cs
+++ Assets/Code/AI/ShootState.cs
@@ -11,8 +11,9 @@ namespace TankGame.AI
         public ShootState( EnemyUnit owner )
           : base( owner, AIStateType.Shoot )
         {
-            // Add transition back to Follow Target
+            // Add transition back to Patrol and Follow Target.
             AddTransition(AIStateType.FollowTarget);
+            AddTransition(AIStateType.Patrol);
         }
 
         public override void StateActivated()
@@ -28,23 +29,31 @@ namespace TankGame.AI
             // If state is not changed ...
             if (!ChangeState())
             {
-                // Shoot with weapon
+                // Shoot with weapon.
                 Owner.Weapon.Shoot();
             }
         }
 
         /// <summary>
-		/// Attempts to change state from Shoot to Follow Target.
+		/// Attempts to change state from Shoot to Patrol or Follow Target.
 		/// </summary>
 		/// <returns>True, if the state change is successful.
         /// False otherwise.</returns>
         private bool ChangeState()
         {
-            // Bool CanShoot is publicly available
+            // If target is dead ...
+            if ( Owner.Target.Health.CurrentHealth <= 0 ) 
+            {
+                // Forget target & change state to Patrol.
+                Owner.Target = null;
+                return Owner.PerformTransition(AIStateType.Patrol);
+            }
+
+            // Bool CanShoot is publicly available.
             // If weapon can't shoot yet ...
             if ( !Owner.Weapon.CanShoot ) 
             {
-                // Change state to Follow Target
+                // Change state to Follow Target.
                 return Owner.PerformTransition(AIStateType.FollowTarget);
             }
 
